f7a0bd1125e848b4cf8ee6cb1c9bc3431cfaed73
Unmesa <dey1011unmesa@gmail.com>
2023-10-31 13:47:58 +0530

    final

---
+++ chat app 2/client/src/App.js
@@ -0,0 +1,48 @@
+import Chat from "./Chat";
+import './App.css';
+import io from 'socket.io-client';
+import {  useState } from 'react';
+
+const socket =io.connect("http://localhost:3001");
+
+
+function App() {
+  const [username,setUsername]=useState("");
+  const [room,setRoom]=useState('');
+  const [showChat,setShowChat]=useState(false);
+
+
+  const joinRoom =()=>{
+    if(username !=="" && room!==""){
+      socket.emit("join_room",room);
+      setShowChat(true);
+
+    }
+  };
+  return (
+    
+    <div className="App">
+    {!showChat ?(
+    <div className="joinChatContainer">
+     <h3>JOin A Chat</h3>
+     <input type='text'
+     placeholder='Name..'
+      onChange={(event)=>{
+          setUsername(event.target.value);
+        }
+        }/>
+     <input type='text'
+     placeholder='Room Id'
+      onChange={(event)=>{
+        setRoom(event.target.value);
+      }}
+     />
+     <button onClick={joinRoom}>Join A Room</button>
+     </div>):(
+     <Chat socket ={socket} username={username} room={room}/>
+    )}
+    </div>
+  );
+}
+export default App;
+
